66124e549d921520fc20040242da28d2e3697a74
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-19 12:11:36 -0400

    Update Rectangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+for (int r = 0; r < height; r++) {
+            for (int c = 0; c < length; c++) {
+                if ((r == 0 || c == 0) || c == length - 1 || r == height - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
