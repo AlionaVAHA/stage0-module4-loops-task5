e520fc6e63e70b1f2570ab0781af9e5a2c59f2c5
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-19 12:29:15 -0400

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+        for (int r = 0; r < cathetusLength; r++) {
+            for (int c = 0; c < r + 1; c++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
 
     }
 }
